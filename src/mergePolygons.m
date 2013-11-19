f2058dd5f7be6f06249ed10add16dd2617d63d3b
Ming <ming.liu192@gmail.com>
2013-11-18 20:16:04 -0500

    mergePolygons: generating merged vertices struct working except some weird corner cases

--- src/mergePolygons.m
+++ src/mergePolygons.m
@@ -72,6 +72,9 @@ for i=1:size(pixB,1)
     end
 end
 
+
+
+
 %create a new image canvas
 %first put in polyA.im, but translate it so everything is positive after
 %merging with polyB.im 
@@ -107,6 +110,12 @@ for i=1:size(pixA,1)
     end
 end
 
+%translate coordinates of polyA's vertices
+for i=1:size(polyA.vertices,2)
+    polyA.vertices(i).posX = polyA.vertices(i).posX + moveAx;
+    polyA.vertices(i).posY = polyA.vertices(i).posY + moveAy;
+end
+
 %fill with polyB pixels according to mapping table
 for i=1:size(dest,1)
     [rowM colM] = coord2mat(dest(i,:), size(imMerge));
@@ -114,16 +123,92 @@ for i=1:size(dest,1)
     imMerge(rowM, colM, :) = pixB(rowB, colB, :);
 end
 
+%translate and rotate coordinates of polyB's vertices
+for i=1:size(polyB.vertices,2)
+    vxNew = polyB.vertices(i).posX + transX;
+    vyNew = polyB.vertices(i).posY + transY;
+    
+    [vxNew,vyNew,xor,yor,trash] = rotateData(vxNew,vyNew,aX,aY,theta,'clockwise');
+    vxNew = vxNew + moveAx;
+    vyNew = vyNew + moveAy;
+    
+    polyB.vertices(i).posX = round(vxNew);
+    polyB.vertices(i).posY = round(vyNew);
+end
+
+
+
 figure;
 imshow(imMerge);
 
-%generate new vertices data for merged polygon
-%TODO
+%generate new vertices data for merged polygon. First merge all vertices
+verticesMerged = polyA.vertices( 1:vA );
+
+%rotate the vertices list for polyB
+verticesNew = polyB.vertices( vB+1 : end );
+verticesNew = [verticesNew, polyB.vertices( 1:vB )];
+verticesMerged = [ verticesMerged, verticesNew ];
+verticesMerged = [ verticesMerged, polyA.vertices( vA+1 : end ) ];
+
+%then delete/merge vertices
+while (1) 
+    found = 0;
+    i=1;
+    while (~found && i<size(verticesMerged,2))
+        j=i+1;
+        while (~found && j < size(verticesMerged,2))
+            v1 = [verticesMerged(i).posX, verticesMerged(i).posY];
+            v2 = [verticesMerged(j).posX, verticesMerged(j).posY];
+            angsum = verticesMerged(i).angle + verticesMerged(j).angle;
+            dist = 5;
+            %delete vertices that add up to 360
+            if ( inProximity(v1, v2, dist) && angsum < 365 && angsum > 355 )
+                verticesMerged([i j]) = [];
+                found=1;
+            %merge vertices next to each other 
+            elseif ( inProximity(v1, v2, dist) && j==i+1 )
+                v1 = i;
+                v2 = j;
+                verticesMerged(v1).angle = verticesMerged(v1).angle + verticesMerged(v2).angle;
+                verticesMerged(v1).dNext = verticesMerged(v2).dNext;
+                verticesMerged(v1).posX = round(mean( [verticesMerged(v1).posX, verticesMerged(v2).posX] ));
+                verticesMerged(v1).posY = round(mean( [verticesMerged(v1).posY, verticesMerged(v2).posY] ));
+                verticesMerged(v2) = [];
+                found=1;
+            elseif ( inProximity(v1, v2, dist) )
+                warning('vertices in the same location, but cant be merged');
+            end
+            j=j+1;
+        end
+        i=i+1;
+    end
+    if ( ~found )
+        break;
+    end
+end
+
+%compute perimeter
+perimeter = 0;
+for i=1:size(verticesMerged)
+    perimeter = perimeter + verticesMerged(i).dNext;
+end
 
 %return new polygon
-%REMOVE THIS SHIT
-polyMerged = polyA;
+polyMerged = struct('vertices', verticesMerged, 'perimeter', perimeter, 'im', imMerge);
 
 
 end
 
+
+
+function res = inProximity(pt1, pt2, dist)
+    if (norm(pt1-pt2) < dist)
+        res = 1;
+    else
+        res = 0;
+    end
+end
+
+
+            
+            
