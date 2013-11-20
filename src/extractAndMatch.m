b8f1b9618cd788419b6cded00b63c85f2a2b9ade
Ming <ming.liu192@gmail.com>
2013-11-20 01:32:10 -0500

    inserted code for calculating matchContent value

---
+++ src/extractAndMatch.m
@@ -0,0 +1,44 @@
+function [matchScore] = extractAndMatch(imi, imj, verti, verti2, vertj, vertj2)
+    % calculate the difference vector between the given vertices
+    vectori = [verti2.posX-verti.posX verti2.posY-verti.posY];
+    vectorj = [vertj2.posX-vertj.posX vertj2.posY-vertj.posY];
+    
+    % convert into polar coordinates to get the angle with respect to 270
+    % degrees (lining the edges vertically with the first vertex on top)
+    [anglei,di] = cart2pol(vectori(1), vectori(2));
+    [anglej,dj] = cart2pol(vectorj(1), vectorj(2));
+    
+    imiRotated = imrotate(imi, 270-anglei);
+    imjRotated = imrotate(imj, 270-anglej);
+    
+    % map the vertices to new coordinates on the rotated images
+    newVerti = mapCoord(verti.posX, verti.posY, size(imi), 270-anglei);
+    newVerti2 = mapCoord(verti2.posX, verti2.posY, size(imi), 270-anglei);
+    newVertj = mapCoord(vertj.posX, vertj.posY, size(imj), 270-anglej);
+    newVertj2 = mapCoord(vertj2.posX, vertj2.posY, size(imj), 270-anglej);
+    
+    % check that the new difference vectors are vertical
+    newVectori = newVerti - newVerti2;
+    newVectorj = newVertj - newVertj2;
+    assert(newVectori(1) == 0 && newVectorj(1) == 0);
+    
+    % take the shorter of the two edges
+    minEdge = min(newVectori(2),newVectorj(2));
+    
+    extractedEdgei = [];
+    extractedEdgej = [];
+    for index=1:minEdge
+        extractedEdgei(index) = imiRotated(newVerti(1),newVerti(2)+index-1,:);
+        extractedEdgej(index) = imjRotated(newVertj(1),(newVertj(2)+index-1),:);
+    end
+   
+    matchScore = matchContent(extractedEdgei, extractedEdgej);
+end
+
+function newVert = mapCoord(x, y, size, rotateAngle)
+    center = [floor(size(2)/2) floor(size(1)/2)];
+    centerVector = [x-center(1) y-center(2)];
+    [centerAngle,centerD] = cart2pol(centerVector(1), centerVector(2));
+    newCenterVector = pol2cart(centerAngle+rotateAngle, centerD);
+    newVert = [x+newCenterVector(1) y+newCenterVector(2)];
+end
